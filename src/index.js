67645c093e77c081b6819ffea2c74a36a00904a7
Negin <negin@alirezas-mbp.speedport.ip>
2024-01-13 11:10:36 +0100

    first codes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="container">
+      <div className="hi">
+        sdfsdfsvsrververvdfcsdvsdvsjkfvnswenfnoiajgvogsengiofjgvoiefjgviosdjviodfjbviojdf
+      </div>
+      <ul className="hi">
+        <li>jkdkjsjsd</li>
+        <li>jkdkjsjsd</li>
+        <li>jkdkjsjsd</li>
+        <li>jkdkjsjsd</li>
+        <li>jkdkjsjsd</li>
+        <li>jkdkjsjsd</li>
+      </ul>
+      <p>
+        <a href="#">kjdcewsuhfg</a>
+      </p>
+    </div>
   </React.StrictMode>
 );
 
